f882a88f2e94cbb65aeecd28b8479e5c857c9a28
Darven <1926248272@qq.com>
2025-06-27 12:17:54 +0800

    [add] 添加空指针异常

--- Safewind-common/src/main/java/com/safewind/common/exception/GlobalException.java
+++ Safewind-common/src/main/java/com/safewind/common/exception/GlobalException.java
@@ -22,20 +22,22 @@ import java.util.Optional;
 public class GlobalException {
     /**
      * 捕获自定义业务异常
+     *
      * @return
      */
-    @ExceptionHandler({ BizException.class })
+    @ExceptionHandler({BizException.class})
     @ResponseBody
     public Result<Object> handleBizException(HttpServletRequest request, BizException e) {
         log.warn("{} request fail, errorCode: {}, errorMessage: {}", request.getRequestURI(), e.getErrorCode(), e.getErrorMessage());
-        return Result.fail(e.getErrorCode(),e.getErrorMessage());
+        return Result.fail(e.getErrorCode(), e.getErrorMessage());
     }
 
     /**
      * 捕获参数校验异常
+     *
      * @return
      */
-    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseBody
     public Result<Object> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
         // 参数错误异常码
@@ -64,16 +66,17 @@ public class GlobalException {
 
         log.warn("{} request error, errorCode: {}, errorMessage: {}", request.getRequestURI(), errorCode, errorMessage);
 
-        return Result.fail(errorMessage,errorCode);
+        return Result.fail(errorMessage, errorCode);
     }
 
     /**
      * 其他类型异常
+     *
      * @param request
      * @param e
      * @return
      */
-    @ExceptionHandler({ Exception.class })
+    @ExceptionHandler({Exception.class})
     @ResponseBody
     public Result<Object> handleOtherException(HttpServletRequest request, Exception e) {
         log.error("{} request error, ", request.getRequestURI(), e);
@@ -82,13 +85,28 @@ public class GlobalException {
 
     /**
      * 参数检验
+     *
      * @param request,e 请求体和错误
      * @return 返回错误信息和打印日志
-     * */
+     */
     @ExceptionHandler({IllegalArgumentException.class})
     @ResponseBody
-    public Result<Object> handleIllegalArgumentException(HttpServletRequest request,IllegalArgumentException e){
-        log.warn("{} param validated ,",request.getRequestURI(), e);
+    public Result<Object> handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException e) {
+        log.warn("{} param validated ,", request.getRequestURI(), e);
+        return Result.fail(ResultCodeEnum.VALIDATION_FAILED, e.getMessage());
+    }
+
+    /**
+     * @param: request 请求
+     * @param: e 参数
+     * @return Result<Object>
+     * @author Darven
+     * @date 2025/6/27 12:12
+     * @description:
+     */
+    @ExceptionHandler({NullPointerException.class})
+    public Result<Object> handleNullPointerException(HttpServletRequest request, NullPointerException e) {
+        log.error("{},param is null",request.getRequestURI(),e);
         return Result.fail(ResultCodeEnum.VALIDATION_FAILED,e.getMessage());
     }
 }
